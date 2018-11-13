dc566f6bdd7b1081aaac3d9b86603264264026ee
Juan Elfers <esjuanma@hotmail.com>
2018-11-13 13:14:25 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,775 @@
+
+	var presentationSlide = true;
+
+	$(document).ready(function () {
+
+		FirstLoad();
+		PageAnim();
+		PageShare();
+		HeroHeight();
+		OpenAbout();
+		ProjectExpander();
+		FullPage();
+		ClassicSlider();
+		MagnificPopup();
+		Shortcodes();
+		ContactForm();
+		AboutUsFix();
+
+	});
+
+	$(window).on('resize', function () {
+		HeroHeight();
+	});
+
+	$(window).scroll(function () {
+		HeroParallax();
+	});
+
+	var Timer = function() {
+		var timer = 0;
+		return function(time) {
+			if(!isNaN(time)) timer += time;
+			return timer;
+		}
+	};
+
+	/*--------------------------------------------------
+	Function Firs tLoad
+	---------------------------------------------------*/
+
+	function FirstLoad() {
+
+		$('body').jpreLoader({
+			loaderVPos: '0',
+			autoClose: false,
+		});
+
+		setTimeout(function () {
+			$('.loader-overlay').fadeOut();
+		}, 1500);
+
+		$('header').removeClass('hidden');
+
+		$('body').waitForImages({
+			finished: function () {
+				setTimeout(function () {
+					$("#hero").removeClass("hidden");
+				}, 1000);
+			},
+			waitForAll: true
+		});
+
+	} // End First Load
+
+
+
+	/*--------------------------------------------------
+	Function Page Animation
+	---------------------------------------------------*/
+
+
+	function PageAnim() {
+
+		$(".animsition").animsition({
+			inClass: 'fade-in',
+			outClass: 'fade-out',
+			inDuration: 150,
+			outDuration: 400,
+			linkElement: '.animation-link',
+			loading: true,
+			loadingParentElement: 'body',
+			loadingClass: 'animsition-loading',
+			loadingInner: '',
+			timeout: true,
+			timeoutCountdown: 15000,
+			onLoadEvent: true,
+			browser: ['animation-duration', '-webkit-animation-duration'],
+			overlay: false,
+			overlayClass: 'animsition-overlay-slide',
+			overlayParentElement: 'body',
+			transition: function (url) { window.location.href = url; }
+		});
+
+	} // End Page Animation
+
+
+
+	/*--------------------------------------------------
+	Function Page Share
+	---------------------------------------------------*/
+
+	function PageShare() {
+
+		$('#page-content').append('<share-button class="share-button"></share-button>');
+
+		setTimeout(function () {
+			config = {
+				networks: {
+					pinterest: {
+						enabled: false
+					},
+					googlePlus: {
+						enabled: false
+					},
+					reddit: {
+						enabled: false
+					},
+					linkedin: {
+						enabled: false
+					},
+					whatsapp: {
+						enabled: false,
+						url: 'http://google.com/'
+					},
+					email: {
+						enabled: true,
+						address: 'monika.a.waszkiewicz@gmail.com',
+						subject: '',
+						body: ''
+					}
+				}
+			}
+
+			var share = new ShareButton('.share-button', config);
+
+		}, 1000);
+
+	} // End PageShare
+
+
+
+	/*--------------------------------------------------
+	Function Hero Height
+	---------------------------------------------------*/
+
+	function HeroHeight() {
+
+		$('#hero').css('height', window.innerHeight + 'px');
+
+	} // End Hero Height
+
+
+
+	/*--------------------------------------------------
+	Function Hero Parallax
+	---------------------------------------------------*/
+
+	function HeroParallax() {
+		// if( block_intro.length > 0 ) var block_intro_top = block_intro.offset().top;	
+
+		// var hero_height = $('#hero-styles').height();
+
+		if ($('#hero-styles').hasClass('parallax-hero')) {
+			var block_intro = $('#hero-styles');
+			var current_top = $(document).scrollTop();
+			block_intro.css('transform', 'translate3d(0, ' + current_top * 0.5 + 'px, 0)');
+		}
+
+	} // End Hero Parallax			
+
+
+
+	/*--------------------------------------------------
+	Function Open About
+	---------------------------------------------------*/
+
+	function OpenAbout() {
+
+		function toggle(scroll) {
+			$.fn.fullpage.setAllowScrolling(scroll);
+			$(".about-open").toggleClass("hidden");
+			$(".about-close").toggleClass("hidden");
+			$("#about-us").toggleClass("is-active");
+		}
+
+		$("li.about-open").on('click', function () {
+			toggle(false);
+		});
+
+		$("li.about-close").on('click', function () {
+			toggle(true);
+			$('#about-us').animate({ scrollTop: 0 }, 200);
+		});
+
+	} // End Open About
+
+
+
+	/*--------------------------------------------------
+	Function Progress Bar
+	---------------------------------------------------*/
+
+	function ProgressBar() {
+
+		var getMax = function () {
+			return $(document).height() - $(window).height();
+		}
+
+		var getValue = function () {
+			return $(window).scrollTop();
+		}
+
+		if ('max' in document.createElement('progress')) {
+			var progressBar = $('progress');
+
+			progressBar.attr({ max: getMax() });
+
+			$(document).on('scroll', function () {
+				progressBar.attr({ value: getValue() });
+			});
+
+			$(window).resize(function () {
+				progressBar.attr({ max: getMax(), value: getValue() });
+			});
+		}
+		else {
+			var progressBar = $('.progress_bar'),
+				max = getMax(),
+				value, width;
+
+			var getWidth = function () {
+				value = getValue();
+				width = (value / max) * 100;
+				width = width + '%';
+				return width;
+			}
+
+			var setWidth = function () {
+				progressBar.css({ width: getWidth() });
+			}
+
+			$(document).on('scroll', setWidth);
+			$(window).on('resize', function () {
+				max = getMax();
+				setWidth();
+			});
+		}
+
+		$('#flat').addClass("active");
+		$('#progressBar').addClass('flat');
+
+		$('#flat').on('click', function () {
+			$('#progressBar').removeClass().addClass('flat');
+			$('a').removeClass();
+			$(this).addClass('active');
+			$(this).preventDefault();
+		});
+
+
+	} // End Progress Bar	
+
+
+
+	/*--------------------------------------------------
+	Function Show Hide Header
+	---------------------------------------------------*/
+
+	var ShowHideHeader = (function() {
+		var interval;
+		var checkInterval;
+		var onScroll;
+
+		ProjectExpander.cleanProjectEvents = function() {
+			clearInterval(interval);
+			clearInterval(checkInterval);
+			$(window).off('scroll', onScroll);
+		};
+
+		return function ShowHideHeader () {
+			var $header = $('header');
+			var $closeButton = $('#project_close');
+			var $window = $(window);
+	
+			var didScroll;
+			var lastScrollTop = 0;
+			var navbarHeight = $header.outerHeight() - 140;
+				
+			// Defines callback
+			onScroll = function (event) {
+				didScroll = true;
+	
+				var blackHeaderShow = $window.scrollTop() >= 400;
+				$header.toggleClass('black-header', blackHeaderShow);
+				$closeButton.toggleClass('black-header', blackHeaderShow);
+			};
+	
+			// Creates interval loop
+			interval = setInterval(function () {
+				if (didScroll) {
+					hasScrolled();
+					didScroll = false;
+				}
+			}, 50);
+
+			checkInterval = setInterval(function () {
+				if ($(this).scrollTop() < 20) {
+					scrollUp();
+				}
+			}, 700);
+	
+			// Add event listener
+			$(window).on('scroll', onScroll);
+	
+			function scrollUp() {
+				// Scroll Up
+				$header.removeClass('nav-up').addClass('nav-down');
+				$closeButton.removeClass('nav-up').addClass('nav-down');
+			}
+
+			function scrollDown() {
+				// Scroll Down
+				$header.removeClass('nav-down nav-up-temp').addClass('nav-up');
+				$closeButton.removeClass('nav-down').removeClass('nav-up-temp').addClass('nav-up');
+			}
+
+			// Helper fn
+			function hasScrolled() {
+				var scrollTop = $(this).scrollTop();
+				if (scrollTop < 0) {
+					scrollTop = 0;
+				}
+
+				if (Math.abs(lastScrollTop - scrollTop) <= 5 && scrollTop > 5) {
+					return;
+				}
+	
+				if(scrollTop === 0) {
+					scrollUp();
+				} else if (scrollTop > lastScrollTop && scrollTop > navbarHeight) {
+					scrollDown();
+				} else if (scrollTop + $(window).height() < $(document).height()) {
+					scrollUp();
+				}
+	
+				lastScrollTop = scrollTop;
+			}
+	
+		}
+	}()) // End Show Hide Header
+
+	function projectNext() {
+		var $projectNext = $('#project_next');
+
+		///Init scripts here
+		if (!$projectNext.length) return;
+		
+		// Since mobile changes the height when URL bar is hidden, we need to use screen.height
+		$projectNext.css('height', (
+			window.innerWidth < 1024
+				? window.screen.height // for mobile
+				: window.innerHeight // for desktop / tablet
+		) + "px");
+
+		$(".project-link").on({
+			mouseenter: function () {
+				$projectNext.addClass("hover");
+			},
+			mouseleave: function () {
+				$projectNext.removeClass("hover");
+			}
+		});
+
+		$("#project_close").on({
+			mouseenter: function () {
+				$('#menu').addClass("hover");
+			},
+			mouseleave: function () {
+				$('#menu').removeClass("hover");
+			}
+		});
+	}
+
+	function projectLinkClick () {
+		var timer = new Timer();
+
+		$('#menu').addClass('hidden');
+		$('#progressBar').addClass('active');
+		$.fn.fullpage.setAllowScrolling(false);
+
+		var openProject = $(this).find('.open-project');
+		var myUrl = (openProject.data('first') || openProject.attr('href')) + " .item-data";
+
+		$('.disable-section').addClass('active');
+
+		setTimeout(function () {
+			$('#loader-line-box').addClass('is-open');
+		}, timer(200));
+	
+		setTimeout(function () {
+			$("#project-page-data").load(myUrl, function (e) {
+				$('#project-page-holder').waitForImages({
+					finished: finished,
+					waitForAll: true
+				});
+			});
+		}, timer(300));
+
+		var finished = function () {
+			var timer = new Timer();
+			projectNext();
+			ClassicSlider();
+			Shortcodes();
+			MagnificPopup();
+
+			setTimeout(function () {
+				$('#project-page-data').addClass('is-open');
+			}, timer(500));
+
+			setTimeout(function () {
+				$('#loader-line-box').removeClass('is-open');
+				$('#fp-nav').addClass('hidden');
+				$('.section-info').addClass('hidden');
+				$('#showcase-nav').addClass('hidden');
+				$('.share-button').addClass('hidden');
+				$('#showcase-slider').removeClass('delay');
+			}, timer(300));
+
+			setTimeout(function () {
+				$('#project-page-holder, #project-page-data').height($('.project-page').height());
+			}, timer(200));
+
+			setTimeout(function () {
+				$("#project_close").removeClass("hide");
+			}, timer(200));
+			
+			setTimeout(function () {
+				$('html, body').animate({ scrollTop: 400 }, 800);
+				$('#project_next').footerReveal({ shadow: false, zIndex: 0 });
+				ProgressBar();
+				ShowHideHeader();
+			}, timer(600));
+		};
+
+		return false;
+	}
+
+	function projectCloseClick () {
+		var $header = $('header');
+		var $closeButton = $('#project_close');
+		$header.removeClass('nav-down nav-up-temp').addClass('nav-up');
+		$closeButton.removeClass('nav-down').removeClass('nav-up-temp').addClass('nav-up');
+		
+		$('#showcase-slider').removeClass('expanded');
+		$('html, body').animate({ scrollTop: 0 }, 800);
+		$.fn.fullpage.setAllowScrolling(true);
+		$('#menu').removeClass('hover');
+		$('.section-info').removeClass('hidden');
+		
+		setTimeout(function () {
+			$('.hamburger').removeClass('hidden');
+		}, 500);
+		
+		setTimeout(function () {
+			$("#project_close").addClass("hide");
+			$('#menu').removeClass('hidden');
+			$('#fp-nav').removeClass('hidden');
+			$('#showcase-nav').removeClass('hidden');
+			$('.share-button').removeClass('hidden');
+			$('#showcase-slider').addClass('delay');
+		}, 1000);
+
+		setTimeout(function () {
+			$('.blog-right').removeClass('active');
+		}, 1300);
+
+		setTimeout(function () {
+			$('#project-page-data').removeClass('is-open');
+			$('#project-page-holder, #project-page-data').height('0');
+			$(".project-page").remove();
+		}, 1100);
+
+		setTimeout(function () {
+			$('.blog-left').removeClass('inactive');
+			$('.disable-section').removeClass('active');
+			ProjectExpander.cleanProjectEvents();
+		}, 1550);
+
+		return false;
+	}
+
+	function projectGeneralLinkClick () {
+		$('#showcase-slider').addClass('expanded');
+		
+		if(presentationSlide && $('.fp-section.active').is(':last-child')) {
+			$.fn.fullpage.moveTo(2);
+		} else {
+			$.fn.fullpage.moveSectionDown();
+		}
+		
+		$("#project_next").addClass("disabled");
+		$('#progressBar').removeClass('active');
+		$(".next-project-footer").addClass("disabled");
+		$('html, body').animate({ scrollTop: $("#nav-anchor").offset().top }, 200);
+		$("#project_close").removeClass("hide");
+		
+		setTimeout(function () {
+			$('header').addClass('nav-up-temp');
+			$('#project_close').addClass('nav-up-temp');
+		}, 220);
+		
+		setTimeout(function () {
+			$('html, body').animate({ scrollTop: 0 }, 0);
+		}, 300);
+		
+		setTimeout(function () {
+			$('#showcase-slider .section.active a.open-project').trigger('click');
+		}, 600);
+
+		return false;
+	}
+
+	/*--------------------------------------------------
+	Function Project Expander
+	---------------------------------------------------*/
+
+	function ProjectExpander() {
+
+		$('#showcase-slider .section .open-project-link').on('click', projectLinkClick);
+		$(document).on('click', '#project_close', projectCloseClick);
+		$(document).on('click', '.project-link', projectGeneralLinkClick);
+
+	} // End Project Expander
+
+
+
+	/*--------------------------------------------------
+	Function FullPage
+	---------------------------------------------------*/
+
+	function FullPage() {
+
+		if (!$('#showcase-slider').length) return;
+
+		$('#showcase-slider').fullpage({
+			css3: true,
+			navigation: true,
+			loopBottom: true,
+			loopTop: true,
+			navigationPosition: 'right',
+			onLeave: function (index, nextIndex, direction) {
+
+				$(window).scroll(function () {
+					if(!$(".active").hasClass("video")) return;
+
+					if ($(window).scrollTop() >= $('#hero').height() - 150) {
+						video.pause();
+					} else {
+						video.play();
+					}
+				});
+			}
+		});
+
+		$.fn.fullpage.setKeyboardScrolling(false);
+
+		$('.showcase-prev').on('click', function () {
+			$.fn.fullpage.moveSectionUp();
+		});
+
+		$('.showcase-next').on('click', function () {
+			$.fn.fullpage.moveSectionDown();
+		});
+
+	} //End FullPage
+
+
+
+	/*--------------------------------------------------
+	Function Classic Slider
+	---------------------------------------------------*/
+
+	function ClassicSlider() {
+
+		$('.classic-slider').flexslider({
+			animation: "slide",
+			direction: "horizontal",
+			animationSpeed: 1000,
+			animationLoop: true,
+			smoothHeight: false,
+			controlNav: false,
+			slideshow: false,
+		});
+
+	} //End ClassicSlider
+
+
+
+	/*--------------------------------------------------
+	Function MagnificPopup
+	---------------------------------------------------*/
+
+	function MagnificPopup() {
+
+		var mfp = $('.mfp-gallery');
+		if (!mfp.length) return;
+		mfp.each(function (index, element) {
+			$(element).magnificPopup({
+				delegate: 'a.mf-zoom',
+				type: 'image',
+				image: {
+					markup: '<div class="mfp-figure">' +
+						'<div class="mfp-close"></div>' +
+						'<div class="mfp-img"></div>' +
+						'</div>' +
+						'<div class="mfp-bottom-bar">' +
+						'<div class="mfp-title"></div>' +
+						'<div class="mfp-counter"></div>' +
+						'</div>',
+
+					cursor: 'mfp-zoom-out-cur',
+
+					verticalFit: true, // Fits image in area vertically
+
+					tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+				},
+				gallery: {
+					enabled: true,
+					tCounter: '<span class="mfp-counter">%curr% / %total%</span>' // markup of counter
+				},
+				mainClass: 'mfp-zoom-in',
+				tLoading: '',
+				removalDelay: 300, //delay removal by X to allow out-animation
+				callbacks: {
+					imageLoadComplete: function () {
+						var self = this;
+						setTimeout(function () {
+							self.wrap.addClass('mfp-image-loaded');
+						}, 16);
+					},
+					close: function () {
+						this.wrap.removeClass('mfp-image-loaded');
+					}
+				},
+				closeBtnInside: false,
+				closeOnContentClick: true,
+				midClick: true
+			});
+		});
+
+	} // End MagnificPopup
+
+
+
+	/*--------------------------------------------------
+	Function Shortcodes
+	---------------------------------------------------*/
+
+	function Shortcodes() {
+
+		//Progress bar animations	
+		$('.progress-bar li').each(function (i) {
+			$(this).appear(function () {
+				var percent = $(this).find('span').attr('data-width');
+				var $endNum = parseInt($(this).find('span strong i').text(), 10);
+				var $that = $(this);
+				$(this).find('span').animate({
+					'width': percent + '%'
+				}, 1600, function () {
+				});
+				$(this).find('span strong').animate({
+					'opacity': 1
+				}, 1400);
+				$(this).find('span strong i').countTo({
+					from: 0,
+					to: $endNum,
+					speed: 1200,
+					refreshInterval: 30,
+					onComplete: function () { }
+				});
+				if (percent == '100') {
+					$that.find('span strong').addClass('full');
+				}
+			});
+		});
+
+		// Milestone counters
+		if (jQuery('.clapat-counter').length > 0) {
+			$('.clapat-counter').each(function () {
+				$(this).appear(function () {
+					var $endNum = parseInt($(this).find('.number').text(), 10);
+					$(this).find('.number').countTo({
+						from: 0,
+						to: $endNum,
+						speed: 1500,
+						refreshInterval: 30
+					});
+				}, { accX: 0, accY: 0 });
+			});
+		}
+
+		// Text Carousel
+		if ($('.text-carousel').length > 0) {
+			$(".text-carousel").owlCarousel({
+				loop: true,
+				dots: true,
+				dotsEach: 1,
+				items: 1,
+				autoplay: true,
+				smartSpeed: 750,
+				autoplayHoverPause: true
+			});
+		}
+
+		// Appear Item Animation
+		$('.has-animation').each(function () {
+			$(this).appear(function () {
+				$(this).delay($(this).attr('data-delay')).queue(function (next) {
+					$(this).addClass('animate-in');
+					next();
+				});
+			});
+
+		});
+
+	} //End Shortcodes
+
+
+
+
+	/*--------------------------------------------------
+	Function Contact Formular
+	---------------------------------------------------*/
+
+	function ContactForm() {
+
+		if (!jQuery('#contact-formular').length) return;
+
+		$('#contactform').submit(function () {
+			var action = $(this).attr('action');
+			$("#message").slideUp(750, function () {
+				$('#message').hide();
+				$('#submit').attr('disabled', 'disabled');
+				$.post(action, {
+					name: $('#name').val(),
+					email: $('#email').val(),
+					comments: $('#comments').val()
+				},
+					function (data) {
+						document.getElementById('message').innerHTML = data;
+						$('#message').slideDown('slow');
+						$('#contactform img.loader').fadeOut('slow', function () { $(this).remove() });
+						$('#submit').removeAttr('disabled');
+						if (data.match('success') != null) $('#contactform').slideUp('slow');
+
+					}
+				);
+			});
+			return false;
+		});
+		
+	} // End ContactForm
+
+
+	/*--------------------------------------------------
+	Fix about-us showing up on some devices
+	---------------------------------------------------*/
+
+	function AboutUsFix() {
+
+		setTimeout(function() {
+			$('#about-us').addClass('ready');
+		}, 400);
+		
+	} // End About us fix
+	
---
+++ readme.txt
@@ -0,0 +1,90 @@
+1. Once page refreshed (clicked on the logo on the homepage) the white page flickers for a sec again.
+
+##### Answer #####
+No new answer for this because I still can't see the flicker.
+##### Answer #####
+
+2. Black bar navigation - Often once inside of the project page (somewhere in the middle) when I click on the close button to leave the page, the black bar stays instead of disappearing which is a very undesired effect.
+See png for reference. It happens in all the browsers: Chrome, Safari, Firefox, etc.
+Also, the close button is not position correctly. Pls see the video for ref.
+
+——
+Mmmm, there is a problem because now the logo and “about” is gone once clicked on the “close”
+button in the black nav.
+
+##### Answer #####
+I fixed a header issue after the first close; so I think this point the same thing
+##### Answer #####
+
+
+——
+So basically, the last project will always link to the first one?
+no matter what that project is, right now?
+or i need to manually change the js?
+Could you just make it so the last project always links to first project
+which is essentially #2 not #1 since that’s the intro page.
+
+##### Answer #####
+- Yes. Last one always links to seconds one, that's it.
+##### Answer #####
+
+
+
+4. Can you relink the envelope from https://fontawesome.com/icons/envelope?style=solid because it doesn’t load in some of my computers even though all my browsers are up to date. Can you also point me to the line of code where i’m able to change the icon if i decide to in the future. 
+
+—-
+Unfortunately it still does not work :( I don’t see the envelope, only an outline
+of a square shape. In all the browsers
+
+##### Answer #####
+ok.. I've made a little change on share.css. I think I've done that before, but well.
+On line 3 of share.css there was an unnecessary space; maybe that was the problem.
+If not, then we will need to change the FA for an image, sorry. 
+##### Answer #####
+
+
+
+4. Once site is uploaded I don’t want (in the url bar) to have the url ending with .html
+I need it to look like this ——   website.com  not website.com/index.html or 
+website.com/michaelkors instead of website.com/michaelkors.html So if i place the project pages html into designated folders will it achieve desired effect?
+
+—-
+So do I need to put the project pages in:
+example: “Fuse.html” file in the individual folder named “Fuse”?
+Will that do the trick? Or i don’t need to do anything and don’t create individual folders?
+
+##### Answer #####
+- You need to do nothing here.
+At least I always see the same page and the URL doesn't change,
+because the content is loaded dinamically without reloading. (and that URL is without the .html of index)
+##### Answer #####
+
+
+
+5. Do you know how to get rid off the “marching ants” (white dotted lines) around the project slides in some of the browsers such as firefox?
+
+ok.. I've misunderstood this one.
+I thought you were talking about the common outline on images and links that are default
+by some browsers, but you were talking about the border during the images transitions I guess.
+I'm sorry, but I wasn't able to fin a solution to that; the only thing I can think of is to
+changing the transition to something simpler and faster, so that issue won't show up or
+won't be visible. It just depends on how well the browser can render the rescalling of the image
+
+That really sucks because I was really hoping to find a solution to that. 
+
+##### Answer #####
+No answer for this yet.
+##### Answer #####
+
+
+
+Also, can the “close” button on the project page disappear to the right instead of the top
+as it was previously? I did not want to change the behavior of that.
+It was way smoother before. pls see videos for reference.
+You can see how when it goes to the top its not aligned with the “about”
+and it’s completely off. 
+
+##### Answer #####
+Issues of videos 1081 and 1082 fixed.
+Video 1083 is not quite clear, but seems the same issue.
+##### Answer #####
